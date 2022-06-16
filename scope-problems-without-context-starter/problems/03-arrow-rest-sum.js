8fe922c37e2bc892b03bcbc7b023a2846ec1061f
rashawn <rcheek513@gmail.com>
2022-06-16 17:29:24 -0400

    number 03

--- scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
+++ scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
@@ -11,6 +11,16 @@ arrowRestSum(0); // => 0
 ***********************************************************************/
 
 // your code here!
+const arrowRestSum = (...nums) => {
+  let sum = 0
+  nums.forEach(function(num){
+    sum += num
+  })
+  return sum
+}
+console.log(arrowRestSum(3,5,6)); // => 14
+console.log(arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 14
+console.log(arrowRestSum(0)); // => 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
